d69b468b540d0f028bf5f4c9a323f2805d761597
Alejandro Betancur <alejobetancur2@gmail.com>
2024-02-29 11:12:33 -0500

    add new functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,133 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
-import { getAllNumbers } from './firebase/firebase'
+import { createRealEstate, getRealEstates } from './firebase/firebase'
+
+const initialData = {
+  name: '',
+  sector: '',
+  price: '',
+  area: '',
+  rooms: '',
+  bathrooms: '',
+  parking: '',
+  address: '',
+  type: '',
+  images: []
+}
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  getAllNumbers();
+  const [ realEstate, setRealEstate ] = useState(initialData);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createRealEstate(realEstate);
+  }
+
+  const handleNameChange = (e) => {
+    setRealEstate({ ...realEstate, name: e.target.value });
+  }
+
+  const handleSectorChange = (e) => {
+    setRealEstate({ ...realEstate, sector: e.target.value });
+  }
+
+  const handlePriceChange = (e) => {
+    setRealEstate({ ...realEstate, price: e.target.value });
+  }
+
+  const handleAreaChange = (e) => {
+    setRealEstate({ ...realEstate, area: e.target.value });
+  }
+
+  const handleRoomsChange = (e) => {
+    setRealEstate({ ...realEstate, rooms: e.target.value });
+  }
+
+  const handleBathroomsChange = (e) => {
+    setRealEstate({ ...realEstate, bathrooms: e.target.value });
+  }
+
+  const handleParkingChange = (e) => {
+    setRealEstate({ ...realEstate, parking: e.target.value });
+  }
+
+  const handleAddressChange = (e) => {
+    setRealEstate({ ...realEstate, address: e.target.value });
+  }
+
+  const handleTypeChange = (e) => {
+    setRealEstate({ ...realEstate, type: e.target.value });
+  }
+
+  const handleImagesChange = (e) => {
+    // Obtén las imágenes seleccionadas por el usuario
+    const files = e.target.files;
+    
+    // Crea una copia del arreglo de imágenes actual
+    const existingImages = [...realEstate.images];
+    
+    // Agrega las nuevas imágenes al arreglo existente
+    for (let i = 0; i < files.length; i++) {
+      existingImages.push(files[i]);
+    }
+
+    // Actualiza el estado con las imágenes combinadas
+    setRealEstate({ ...realEstate, images: existingImages });
+  }
+
+  getRealEstates();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Crear nueva propiedad */}
+      <form className='form' onSubmit={ handleSubmit } >
+        <label>
+          Nombre:
+          <input type="text" name="name" onChange={ handleNameChange } />
+        </label>
+        <label>
+          Sector de la vivienda:
+          <input type="text" name="sector" onChange={ handleSectorChange } />
+        </label>
+        <label>
+          Precio:
+          <input type="text" name="price" onChange={ handlePriceChange } />
+        </label>
+        <label>
+          Área:
+          <input type="text" name="area" onChange={ handleAreaChange } />
+        </label>
+        <label>
+          Habitaciones:
+          <input type="text" name="rooms" onChange={ handleRoomsChange } />
+        </label>
+        <label>
+          Baños:
+          <input type="text" name="bathrooms" onChange={ handleBathroomsChange } />
+        </label>
+        <label>
+          Parqueadero:
+          <input type="text" name="parking" onChange={ handleParkingChange } />
+        </label>
+        <label>
+          Dirección:
+          <input type="text" name="address" onChange={ handleAddressChange } />
+        </label>
+        <label>
+          Tipo de vivienda:
+        </label>
+        <select name="" onChange={ handleTypeChange }>
+          <option value="">Seleccione</option>
+          <option value="casa">Casa</option>
+          <option value="apartamento">Apartamento</option>
+          <option value="lote">Lote</option>
+        </select>
+        {/* Cargar imagenes */}
+        <p>Cargar imagenes:</p>
+        <input className='img_input' type="file" name="file" id="file" onChange={ handleImagesChange } />
+        <input type="submit" value="Enviar" />
+      </form>
     </>
   )
 }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 import {
-    getFirestore,
-    collection,
-    getDocs,
-} from "firebase/firestore";
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2z9a0VNRBmbV5kMe_8Qp4vAr9o-E292U",
@@ -11,19 +13,68 @@ const firebaseConfig = {
   projectId: "pruebas-inmuebles",
   storageBucket: "pruebas-inmuebles.appspot.com",
   messagingSenderId: "530118818996",
-  appId: "1:530118818996:web:28b92a0d34561d831c2be1"
+  appId: "1:530118818996:web:28b92a0d34561d831c2be1",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 
+const createId = () =>
+  Math.random().toString(36).substring(2) + Date.now().toString(36);
+
 // Consultar la collection numbers de la base de datos para obtener todos los documentos
-export const getAllNumbers = async () => { 
-    const querySnapshot = collection(db, 'numbers');
-    const querySnapshotData = await getDocs(querySnapshot);
-    
-    const numbers = querySnapshotData.docs.map((doc) => {
-        return doc.data();
-    });
-    console.log(numbers);
+export const getAllNumbers = async () => {
+  const querySnapshot = collection(db, "numbers");
+  const querySnapshotData = await getDocs(querySnapshot);
+
+  const numbers = querySnapshotData.docs.map((doc) => {
+    return doc.data();
+  });
+  console.log(numbers);
 };
+
+// Consultar la collection realEstates de la base de datos para obtener todos los documentos
+export const getRealEstates = async () => {
+  const querySnapshot = collection(db, "realEstates");
+  const querySnapshotData = await getDocs(querySnapshot);
+
+  const realEstates = querySnapshotData.docs.map((doc) => {
+    return doc.data();
+  });
+
+  console.log(realEstates);
+};
+
+// Crear un nuevo documento en la collection realEstates.
+export const createRealEstate = async (data) => {
+    try {
+      const id = createId();
+      const storage = getStorage();
+      const storageRef = ref(storage, `images/${id}`);
+  
+      // Subir todas las imágenes en paralelo al storage
+      const uploadPromises = data.images.map(async (image) => {
+        const imageRef = ref(storageRef, `${id}_${image.name}`);
+        await uploadBytesResumable(imageRef, image);
+        return getDownloadURL(imageRef);
+      });
+  
+      // Esperar a que todas las imágenes se suban y obtengan sus URLs
+      const imageUrls = await Promise.all(uploadPromises);
+  
+      // Crear el objeto realEstate con las URLs de las imágenes
+      const realEstate = {
+        ...data,
+        id,
+        images: imageUrls,
+      };
+  
+      // Agregar el nuevo documento a Firestore
+      await addDoc(collection(db, "realEstates"), realEstate);
+  
+      console.log("Propiedad creada exitosamente:", realEstate);
+    } catch (error) {
+      console.error("Error al crear la propiedad:", error);
+    }
+  };
+  
